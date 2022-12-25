f26c608efd7b41431dd8352ecf5733106f27deaf
Julio Tassinari <tassinarijulio@gmail.com>
2022-12-25 20:54:00 -0300

    Updated Firebase init

--- client/src/firebase/init.ts
+++ client/src/firebase/init.ts
@@ -14,7 +14,9 @@ const firebaseConfig = {
 	projectId: import.meta.env.VITE_FB_PROJECT_ID,
 	storageBucket: import.meta.env.VITE_FB_STORAGE_BUCKET,
 	messagingSenderId: import.meta.env.VITE_FB_MESSAGING_SENDER_ID,
-	appId: import.meta.env.VITE_FB_APP_ID
+	appId: import.meta.env.VITE_FB_APP_ID,
+	measurementId: import.meta.env.VITE_FB_MEASURE_ID
+
 }
 
 // Initialize Firebase
